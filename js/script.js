fe508f20bdaae3918b3ee3ac6d0af6bff19e93a9
thiagoStella <thiagostellapontes01@gmail.com>
2024-03-26 16:36:14 -0300

    responsividade, social

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ let navlist = document.querySelector('.navlist');
 
 function clickMenu(){
     if (navlist.style.display == 'block') {
-        navlist.style.display = 'none'
+        navlist.style.display = 'none';
     }else{
-        navlist.style.display = 'block'
+        navlist.style.display = 'block';
     }
 }
 
